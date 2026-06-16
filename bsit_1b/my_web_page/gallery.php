7d22af733de273bd7e8b46e9d9301bbb56642c27
sesdoyrodev <s2501292@usls.edu.ph>
2026-06-16 13:03:10 +0800

    Add files via upload

---
+++ bsit_1b/my_web_page/gallery.php
@@ -0,0 +1,9 @@
+<h1 align="center">Gallery</h1>
+				<br>
+				<ul>
+                  <img src= "gallery-1.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery-2.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery-3.jpg" alt="txt" width=250 height=250>
+                  <img src= "gallery-4.jpg" alt="txt" width=250 height=250>
+                </ul>
+    
---
+++ bsit_1b/my_web_page/home.php
@@ -0,0 +1,11 @@
+
+				<h1 align="center">Home</h1>
+				<br>
+                <p><b>Student ID No.:</b> 2501292</p>
+				<p><b>Full Name:</b> Alex Carl P. Sesdoyro</p>
+				<p><b>Program/Degree:</b> BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY</p>
+				<p><b>Year Level:</b> First Year</p>
+				<p><b>Student Status:</b> Old</p>
+                <p><b>Email:</b> s2501292@usls.edu.ph</p>
+				<p><b>Phone Number:</b> (+63) 956 265 9295</p>
+                <p><b>Present/permanent Address:</b> Blk. 10, Paglaum Village Brgy. Mansilingan, Bacolod City</p>
